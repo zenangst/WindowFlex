158b81a263c24378c34f08dea273523cad58a232
Christoffer Winterkvist <chris@zenangst.com>
2014-10-31 21:13:00 +0100

    Initial implementation

--- WindowFlex/WindowFlex.m
+++ WindowFlex/WindowFlex.m
@@ -6,13 +6,46 @@
 //    Copyright (c) 2014 zenangst. All rights reserved.
 //
 
+#import <objc/objc-runtime.h>
 #import "WindowFlex.h"
 
 static WindowFlex *sharedPlugin;
 
+@implementation NSWindowController (ZEN)
+
+- (NSSize)zen_windowWillResize:(NSWindow *)sender
+                        toSize:(NSSize)frameSize
+{
+    if (frameSize.width < 620.0f) {
+        frameSize.width = 620.0f;
+    }
+
+    return frameSize;
+}
+
+@end
+
+@implementation NSToolbarItem (ZEN)
+
+- (NSSize)zen_minSize
+{
+    NSSize size = [self zen_minSize];
+
+    Class activityClass = NSClassFromString(@"_IDEActivityViewControllerToolbarItem");
+
+    if ([self isKindOfClass:activityClass]) {
+        size.width = 320.0f;
+    }
+
+    return size;
+}
+
+@end
+
 @interface WindowFlex()
 
 @property (nonatomic, strong, readwrite) NSBundle *bundle;
+
 @end
 
 @implementation WindowFlex
@@ -40,12 +73,56 @@ static WindowFlex *sharedPlugin;
 
     self.bundle = plugin;
 
+    [self swizzle];
+
     return self;
 }
 
 - (void)dealloc
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)swizzle
+{
+    static dispatch_once_t onceToken;
+
+    Class IDEWorkspaceWindowControllerClass = NSClassFromString(@"IDEWorkspaceWindowController");
+
+    dispatch_once(&onceToken, ^{
+        [self swizzleClass:IDEWorkspaceWindowControllerClass
+          originalSelector:@selector(windowWillResize:toSize:)
+          swizzledSelector:@selector(zen_windowWillResize:toSize:)
+            instanceMethod:YES];
+
+        [self swizzleClass:[NSToolbarItem class]
+          originalSelector:@selector(minSize)
+          swizzledSelector:@selector(zen_minSize)
+            instanceMethod:YES];
+    });
+}
+
+- (void)swizzleClass:(Class)class originalSelector:(SEL)originalSelector swizzledSelector:(SEL)swizzledSelector instanceMethod:(BOOL)instanceMethod
+{
+    if (class) {
+        Method originalMethod;
+        Method swizzledMethod;
+        if (instanceMethod) {
+            originalMethod = class_getInstanceMethod(class, originalSelector);
+            swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        } else {
+            originalMethod = class_getClassMethod(class, originalSelector);
+            swizzledMethod = class_getClassMethod(class, swizzledSelector);
+        }
+
+        BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+
+        if (didAddMethod) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    }
 }
 
 @end
